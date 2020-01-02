0674374b550bcd5f256f33d67327a6864624b4ae
Camilo III P. Ortillo <camortillo@gmail.com>
2020-01-01 21:31:12 -0500

    added initial discord bot files

---
+++ Bot/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const Discord = require('discord.js');
+const bot = new Discord.Client()
+
+bot.commands = new Discord.Collection();
+const botCommands = require('./commands');
+
+Object.keys(botCommands).map(key => {
+    bot.commands.set(botCommands[key].name, botCommands[key]);
+})
+
+const TOKEN = process.env.TOKEN
+const prefix = process.env.PREFIX
+
+bot.login(TOKEN)
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`)
+})
+
+bot.on('message', msg => {
+  if (msg.content.startsWith(prefix)) {
+    const args = msg.content.split(/ +/);
+    const command = args.shift().toLowerCase().substring(prefix.length,);
+
+    if (bot.commands.has(command)) {
+      try {
+        bot.commands.get(command).execute(msg, args);
+      } catch (error) {
+        console.error(error);
+        msg.reply('there was an error trying to execute that command!');
+      }
+    }
+  }
+})
